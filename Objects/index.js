6d353e787b962bac7d40bff8af78418afa8428af
Hubert Lemczak <hubertlemczak@gmail.com>
2022-05-18 11:20:16 +0100

    c

---
+++ Objects/index.js
@@ -0,0 +1,12 @@
+const inbox = {
+  email: 'email123@gmail.com',
+  subject: 'extended car warranty',
+};
+
+console.log(inbox);
+
+inbox.name = 'guy';
+console.log(inbox);
+
+inbox.email = 'newemail321@gmail.com';
+console.log(inbox);
