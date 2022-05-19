842625c2624359547fbd9d3f3c725d9d0bad75a7
Hubert Lemczak <hubertlemczak@gmail.com>
2022-05-19 14:41:38 +0100

    c

--- Arrays/lesson.js
+++ Arrays/lesson.js
@@ -1,14 +1,14 @@
-const myNumbers = [32, 32, 6, 47, 48, 32, 13, 20, 16, 23];
+// const myNumbers = [32, 32, 6, 47, 48, 32, 13, 20, 16, 23];
 
-let product = (array) => {
-  let product = 1;
-  for (i = 0; i < array.length; i++) {
-    product *= array[i];
-  }
-  return product;
-};
+// let product = (array) => {
+//   let product = 1;
+//   for (i = 0; i < array.length; i++) {
+//     product *= array[i];
+//   }
+//   return product;
+// };
 
-console.log(product(myNumbers));
+// console.log(product(myNumbers));
 
 // let product = 1;
 // for (i = 0; i < myNumbers.length; i++) {
---
+++ Functions/index.js
@@ -0,0 +1,33 @@
+// let friend = 'Darkness';
+
+// function greetFriend() {
+//   const otherFriends = ['Kitty', 'Moto', 'is it me you’re looking for?'];
+//   console.log(`Hello ${friend}, my old friend!`);
+
+//   for (const friend of otherFriends) {
+//     console.log(`Hello ${friend}, my old friend!`);
+//   }
+// }
+
+// console.log(greetFriend('fren'));
+
+// const randomNums = [];
+// let getRandomNumber = (min, max, amount) => {
+//   for (let i = 0; i < amount; i++) randomNums.push(Math.round(Math.random() * (max - min) + min));
+//   return randomNums;
+// };
+
+// console.log(getRandomNumber(1, 100, 1));
+
+// let isOddOrEven = () => randomNums.map((num) => (num % 2 ? num + ' Odd' : num + ' Even'));
+
+// console.log(isOddOrEven());
+
+// const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+// console.log(getRandomNumber(1, 100));
+// console.log(getRandomNumber(100, 200));
+
+// const age = 20;
+// if (age < 18) console.log("Come back when you're older!");
+// else if (age > 65) console.log('Enjoy your retirement!');
+// console.log('You may come in.');
