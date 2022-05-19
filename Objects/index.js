8a26058d4ff318084ab775f931062cdd284ab073
Hubert Lemczak <hubertlemczak@gmail.com>
2022-05-19 15:58:25 +0100

    c

--- Objects/index.js
+++ Objects/index.js
@@ -1,12 +1,24 @@
-const inbox = {
-  email: 'email123@gmail.com',
-  subject: 'extended car warranty',
-};
+// const inbox = {
+//   email: 'email123@gmail.com',
+//   subject: 'extended car warranty',
+// };
 
-console.log(inbox);
+// console.log(inbox);
 
-inbox.name = 'guy';
-console.log(inbox);
+// inbox.name = 'guy';
+// console.log(inbox);
 
-inbox.email = 'newemail321@gmail.com';
-console.log(inbox);
+// inbox.email = 'newemail321@gmail.com';
+// console.log(inbox);
+
+// const obj = ['0: 1', '1: 2', '2: 3', 'length: 3'];
+// console.log(obj);
+
+// const reversed = obj.reverse();
+// console.log(reversed);
+
+// const obj = { 0: 1, 1: 2, 2: 3, length: 3 };
+// console.log(obj);
+
+// Array.prototype.reverse.call(obj);
+// console.log(obj);
