63bd8ff54adb68b68528f7bd56c36208420c1fcd
Hubert Lemczak <hubertlemczak@gmail.com>
2022-05-27 17:17:49 +0100

    CLASSES

---
+++ Classes/classes.js
@@ -0,0 +1,14 @@
+class Animal {
+  constructor(type, color) {
+    this.type = type;
+    this.color = color;
+  }
+}
+const animalOne = new Animal('fox', 'blue');
+const animalTwo = new Animal('cat', 'red');
+console.log(animalOne, animalTwo);
+
+const occupancy = () => {
+  const enter = (numAdult, numChild) => {};
+  const leave = () => {};
+};
