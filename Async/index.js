b0acabbc790f625421620a1401adb3e2699846fd
Hubert Lemczak <hubertlemczak@gmail.com>
2022-07-12 10:08:45 +0100

    react-counter

--- Async/index.js
+++ Async/index.js
@@ -4,6 +4,7 @@ const h1 = document.createElement('h1');
 h1.style.fontSize = '4rem';
 h1.innerText = 1;
 document.body.append(h1);
+
 const interval = () => {
   const timer = setInterval(() => {
     h1.innerText--;
@@ -13,17 +14,16 @@ const interval = () => {
     }
   }, 1000);
 };
+
+interval();
+
 let flashInterval;
 const flash = () => {
   flashInterval = setInterval(() => {
     h1.style.color = 'red';
-    setTimeout(() => {
-      h1.style.color = 'black';
-    }, 250);
+    setTimeout(() => (h1.style.color = 'black'), 250);
   }, 500);
 };
-
-interval();
 
 const createResetBtn = () => {
   const btn = document.createElement('button');
@@ -32,10 +32,12 @@ const createResetBtn = () => {
   document.body.append(btn);
 
   btn.addEventListener('click', () => {
-    clearInterval(flashInterval);
-    h1.innerText = 10;
-    h1.style.color = 'black';
-    interval();
+    if (h1.innerText == 0) {
+      clearInterval(flashInterval);
+      h1.innerText = 10;
+      h1.style.color = 'black';
+      interval();
+    }
   });
 };
 
